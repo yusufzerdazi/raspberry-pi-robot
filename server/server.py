c54fdaa6c1151f2a5f7f74f548d426d43ba90be7
Yusuf Zerdazi <yusuf@zerdazi.com>
2017-01-25 13:24:15 +0000

    Implemented RANSAC

--- server/server.py
+++ server/server.py
@@ -3,6 +3,11 @@ import sys
 import math
 import threading
 import time
+import random
+from numpy.linalg import norm
+import numpy as np
+from scipy import stats
+from operator import itemgetter
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PIL import Image, ImageDraw, ImageQt
 
@@ -84,6 +89,7 @@ def plot_state(x, y, heading):
     y_distance = 200 * math.sin(math.radians(heading))
     draw.ellipse((x + WIDTH/2-r, y + HEIGHT/2-r, x + WIDTH/2+r, y + HEIGHT/2+r), fill="red")
     draw.line((x + WIDTH/2, y + HEIGHT/2, x + WIDTH/2 + int(x_distance), y + HEIGHT/2 + int(y_distance)), fill="red")
+    #draw.ellipse((x + WIDTH/2 + int(x_distance), y + HEIGHT/2 + int(y_distance)), fill="black")
 
 
 def measurement_to_coords(start_x, start_y, heading, distance=200, sensor=0):
@@ -95,39 +101,87 @@ def measurement_to_coords(start_x, start_y, heading, distance=200, sensor=0):
     return (start_x+WIDTH/2.0, start_y+HEIGHT/2.0, end_x+WIDTH/2.0, end_y+HEIGHT/2.0, angle)
 
 
-def ransac(points):
-    sorted_points = sorted(L, key=itemgetter(4))
-    
+def ransac(N, number_samples, degree_range, max_distance, number_points, points):
+    n = 0
+    while len(points) > number_points and n < N:
+        index = random.randint(0, len(points)-1)
+        within_angle = [points[i] for i in range(len(points)) if abs((points[index][4] - points[i][4]) % 360) < degree_range]
+        sample = random.sample(within_angle, min(number_samples, len(within_angle)))
+        slope, intercept, r_value, p_value, std_err = stats.linregress([sample[i][2] for i in range(len(sample))], [sample[i][3] for i in range(len(sample))])
+        
+
+        points_on_line = []
+        p1 = np.array([0, intercept])
+        p2 = np.array([WIDTH, slope*WIDTH+intercept])
+        for point in points:
+            p3 = np.array([point[2], point[3]])
+            if norm(np.cross(p2-p1, p1-p3))/norm(p2-p1) < max_distance:
+                points_on_line.append(point)
+
+        if len(points_on_line) > number_points:
+            slope, intercept, r_value, p_value, std_err = stats.linregress([points_on_line[i][2] for i in range(len(points_on_line))], [points_on_line[i][3] for i in range(len(points_on_line))])
+            plot_measurement((0, intercept, WIDTH, slope*WIDTH+intercept))
+            return (0, intercept, WIDTH, slope*WIDTH+intercept)
 
 
-def run(app):
-    p_x = 0
-    p_y = 0
-    measurements = []
-    while RUNNING:
+class Main(threading.Thread):
+    def __init__(self, app):
+        threading.Thread.__init__(self)
+        self.p_x = 0
+        self.p_y = 0
+
+
+    def run(self):
+        #self.ransac_loop()
+        while RUNNING:
+            self.remote_control_loop()
+        SOCK.close()
+        app.quit()
+        return 0
+
+    def ransac_loop(self):
+        SOCK.sendto("C".encode(), (UDP_IP, UDP_PORT))
+        measurements = []
+        while len(measurements) < 100:
+            try:
+                data, address = SOCK.recvfrom(1024)  # buffer size is 1024 bytes
+                x, y, heading, angle, forward, backward = [float(x) for x in data.decode().split(',')]
+                if forward not in [-1, 255]:
+                    measurements.append(measurement_to_coords(x, y, heading, forward, angle/2 + 180))
+                if backward not in [-1, 255]:
+                    measurements.append(measurement_to_coords(x, y, heading, backward, angle/2))
+                #if math.sqrt((x-self.p_x)**2+(y-self.p_y)**2) > 20:
+                plot_state(x, y, heading)
+                #    self.p_x = x
+                #    self.p_y = y
+            except BlockingIOError:
+                pass
+        SOCK.sendto("X".encode(), (UDP_IP, UDP_PORT))
+        
+        for m in measurements:
+            plot_measurement(m)
+        ransac(10, 5, 20, 2, 10, measurements)
+
+    def remote_control_loop(self):
         try:
             data, address = SOCK.recvfrom(1024)  # buffer size is 1024 bytes
             x, y, heading, angle, forward, backward = [float(x) for x in data.decode().split(',')]
             if forward not in [-1, 255]:
-                forward_coords = measurement_to_coords(x, y, heading, forward, angle/2 + 180)
-                plot_measurement(forward_coords)
+                plot_measurement(measurement_to_coords(x, y, heading, forward, angle/2 + 180))
             if backward not in [-1, 255]:
-                rear_coords = measurement_to_coords(x, y, heading, backward, angle/2)
-                plot_measurement(rear_coords)
-            if math.sqrt((x-p_x)**2+(y-p_y)**2) > 20:
-                plot_state(x, y, heading)
-                p_x = x
-                p_y = y
+                plot_measurement(measurement_to_coords(x, y, heading, backward, angle/2))
+            #if math.sqrt((x-self.p_x)**2+(y-self.p_y)**2) > 20:
+            plot_state(x, y, heading)
+            #    self.p_x = x
+            #    self.p_y = y
         except BlockingIOError:
             pass
-    SOCK.close()
-    app.quit()
-    return 0
 
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    threading.Thread(target=run, args=[app]).start()
+    main = Main(app)
+    main.start()
     cv = CameraViewer()
     cv.show()
     sys.exit(app.exec_())
